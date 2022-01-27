0332470748c3b5d0c0804b4f946e9b447530f6d0
Renee Brinkman <reneebrinkman91@gmail.com>
2022-01-27 12:31:05 -0600

    Allow player to play again as many times as they wish without having to restart the program

--- guessing_game.py
+++ guessing_game.py
@@ -12,6 +12,17 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 import random
 
 
+def check_play_again():
+    while True:
+        play_again = input('Do you want to play again? (Yes/No) ')
+
+        if play_again.lower() == 'yes':
+            return True
+        elif play_again.lower() == 'no':
+            return False
+        else:
+            print('Invalid response. Please say "yes" or "no".')
+
 def start_game():
     """Psuedo-code Hints
 
@@ -21,42 +32,51 @@ def start_game():
     """
     print('Welcome to the number guessing game!\nBy Renee Louise Brinkman')
 
-    """2. Store a random number as the answer/solution."""
-    answer = random.randint(1, 10)
+    playing = True
+    while playing:
 
-    """3. Continuously prompt the player for a guess."""
-    guess_correct = False
-    attempts = 0
-    while not guess_correct:
-        try:
-            guess = int(input('Try guessing a number between 1 and 10! '))
-        except ValueError:
-            print('You did not enter a valid number between 1 and 10. Please try again.')
-            continue
 
-        """a. If the guess greater than the solution, display to the player "It's lower".
-        b. If the guess is less than the solution, display to the player "It's higher".
-        """
-        if guess > answer:
-            print("It's lower than that")
-        elif guess < answer:
-            print("It's higher than that")
+        """2. Store a random number as the answer/solution."""
+        answer = random.randint(1, 10)
 
-        else:
-            guess_correct = True
+        """3. Continuously prompt the player for a guess."""
+        guess_correct = False
+        attempts = 0
+        while not guess_correct:
+            try:
+                guess = int(input('Try guessing a number between 1 and 10! '))
+            except ValueError:
+                print('You did not enter a valid number between 1 and 10. Please try again.')
+                continue
 
-        attempts += 1
+            if guess < 1 or guess > 10:
+                print('Your number is out of range. It must be between 1 and 10. Please try again.')
+                continue
 
-    """4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number."""
-    print(f'Yes! You got it! It took you {attempts} tries to guess correctly.')
+            """a. If the guess greater than the solution, display to the player "It's lower".
+            b. If the guess is less than the solution, display to the player "It's higher".
+            """
+            if guess > answer:
+                print("It's lower than that")
+            elif guess < answer:
+                print("It's higher than that")
+
+            else:
+                guess_correct = True
+
+            attempts += 1
+
+        """4. Once the guess is correct, stop looping, inform the user they "Got it"
+             and show how many attempts it took them to get the correct number."""
+        print(f'Yes! You got it! It took you {attempts} tries to guess correctly.')
+
+        playing = check_play_again()
 
     """5. Let the player know the game is ending, or something that indicates the game is over."""
     print('The game is over. Thanks for playing!')
 
     """( You can add more features/enhancements if you'd like to. )
     """
-    # write your code inside this function.
 
 
 
